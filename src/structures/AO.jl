920580593e65da77898426154fadefa926a9efcd
LaurentVidal95 <laurent.vida@gmail.com>
2024-02-13 13:22:55 +0100

    Compute everything with pyscf... Added selection of AOs by mmax

--- src/structures/AO.jl
+++ src/structures/AO.jl
@@ -62,16 +62,63 @@ function AO_basis(X::Element{T};
             end
         end
     end
-    AOs 
+    AOs
 end
 
-function H¹_overlap(X::Element{T}, R; kwargs...) where {T<:Real}
+function eval_abs_ms(mol::PyObject)
+    # Compute AO labels for molecule X₂
+    all_labels = mol.ao_labels(fmt=false)
+    N_aos = div(length(all_labels),2)
+    labels = all_labels[1:N_aos] # AO label on first X
+
+    # Compute abs of the associated quantum number m
+    multipopfirst!(tab, N) = [popfirst!(tab) for x in tab[1:N]]
+    abs_ms = Int[]
+    while !isempty(labels)
+        l = split(labels[1][3],"")[end]
+        n_aos_nl=Dict("s"=>1, "p"=>3, "d"=>5, "f"=>7, "g"=>9,
+                      "h"=>11)
+        aos_nl = multipopfirst!(labels, n_aos_nl[l])
+        if l=="s"
+            append!(abs_ms, [0])
+        elseif l=="p"
+            append!(abs_ms, [1,1,0])
+        else # l=d,f,g,...
+            # Compute numbers of aos with same n and l
+            half_n_ao_nl = div(length(aos_nl), 2)            
+            append!(abs_ms, abs.(collect(-half_n_ao_nl:half_n_ao_nl)))
+        end
+    end
+    vcat(abs_ms, abs_ms)
+end
+
+
+function eval_AOs(grid::QuadGrid, A::Element{T1}, B::Element{T1},
+                  R::T2) where {T1, T2<:Real}
+    basis = Dict([A.name => basis_string([A]),
+                  B.name => basis_string([B])])
+    tmp_mol = pyscf.M(;atom="$(A.name) 0.0 0.0 0.0;
+                             $(B.name) 0.0 0.0 $R",
+                      basis
+                      )
+    X = pyscf.dft.numint.eval_ao(tmp_mol, grid.points)
+    # filter X with mmax: X[:,i]=0 if |m(X[:,i])| > mmax
+    @show abs_ms = eval_abs_ms(tmp_mol)
+    for (im, m) in enumerate(abs_ms)
+        if m>grid.mmax
+            X[:,im] .= zero(X[:,im])
+        end
+    end
+    X
+end
+
+function overlap(X::Element{T}, R; norm_type=:L²) where {T<:Real}
     basis = Dict([X.name => basis_string([X])])
     tmp_mol = pyscf.M(;atom="$(X.name) 0.0 0.0 0.0;
                              $(X.name) 0.0 0.0 $R",
-                      basis,
-                      kwargs...
+                      basis
                       )
+    (norm_type=:L²) && (return tmp_mol.intor("int1e_ovlp"))
     2*tmp_mol.intor("int1e_kin")
 end
 
